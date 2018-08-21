fde018829251b8e5771063536146cbfb8b6f345b
phplessons2018 <s.dimitrenco@webdelo.org>
2018-08-21 12:09:31 +0300

    add code to test.php

--- src/test.php
+++ src/test.php
@@ -1,2 +1,24 @@
 <?php
 
+function calcEverything($log, $numbers)
+{
+    echo implode($log, $numbers) . ' = ';
+
+    $count = 0;
+
+    foreach ($numbers as $key => $value) {
+        if($log == '+') {
+
+            $count += $value;
+        }
+        else {
+            $count = $value;
+        }
+
+    }
+
+    echo $count;
+
+
+
+}
